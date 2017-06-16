2ddf28d9ae38ab2fbf15efa6b6b5c03cff12863a
hikalium <hikalium@hikalium.com>
2017-06-16 15:14:20 +0900

    Update calc.py

--- 03_calc/calc.py
+++ 03_calc/calc.py
@@ -1,3 +1,23 @@
+opList = {
+	"+": {'type': 'OP', 'op': 'ADD', 'prec': 1},		
+	"-": {'type': 'OP', 'op': 'SUB', 'prec': 1},		
+	"*": {'type': 'OP', 'op': 'MUL', 'prec': 2},		
+	"/": {'type': 'OP', 'op': 'DIV', 'prec': 2},		
+	"%": {'type': 'OP', 'op': 'MOD', 'prec': 2},		
+	"^": {'type': 'OP', 'op': 'POW', 'prec': 3},		
+	"(": {'type': '('},		
+	")": {'type': ')'},		
+}
+
+evalTable = {
+	"ADD": lambda L, R : L + R,
+	"SUB": lambda L, R : L - R,
+	"MUL": lambda L, R : L * R,
+	"DIV": lambda L, R : L / R,
+	"MOD": lambda L, R : L % R,
+	"POW": lambda L, R : L ** R,
+}
+
 def readNumber(line, index):
 	number = 0
 	while index < len(line) and line[index].isdigit():
@@ -10,56 +30,98 @@ def readNumber(line, index):
 			number += int(line[index]) * keta
 			keta *= 0.1
 			index += 1
-	token = {'type': 'NUMBER', 'number': number}
+	token = {'type': 'NUMBER', 'val': number}
 	return token, index
-
-
-def readPlus(line, index):
-	token = {'type': 'PLUS'}
-	return token, index + 1
-
-
-def readMinus(line, index):
-	token = {'type': 'MINUS'}
-	return token, index + 1
-
 
 def tokenize(line):
 	tokens = []
 	index = 0
 	while index < len(line):
+		if line[index].isspace():
+			(token, index) = (token, index + 1)
 		if line[index].isdigit():
 			(token, index) = readNumber(line, index)
-		elif line[index] == '+':
-			(token, index) = readPlus(line, index)
-		elif line[index] == '-':
-			(token, index) = readMinus(line, index)
+		elif line[index] in opList :
+			token = opList[line[index]]
+			index = index + 1
 		else:
 			print 'Invalid character found: ' + line[index]
 			exit(1)
 		tokens.append(token)
+	# print tokens
 	return tokens
 
+def parse_checkPrec(evalStack, opStack):
+	if len(opStack) < 2:
+		return
+	if 'prec' not in opStack[-2] or 'prec' not in opStack[-1]:
+		return
+	if opStack[-2]['prec'] >= opStack[-1]['prec']:
+		evalStack.append(opStack.pop(-2))
+		parse_checkPrec(evalStack, opStack)
 
-def evaluate(tokens):
-	answer = 0
-	tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
-	index = 1
-	while index < len(tokens):
-		if tokens[index]['type'] == 'NUMBER':
-			if tokens[index - 1]['type'] == 'PLUS':
-				answer += tokens[index]['number']
-			elif tokens[index - 1]['type'] == 'MINUS':
-				answer -= tokens[index]['number']
+def parse(tokens):
+	evalStack = []
+	opStack = []
+	while tokens:
+		token = tokens.pop(0)
+		if token['type'] == 'NUMBER':
+			evalStack.append(token)
+		else:
+			if token['type'] == 'OP':
+				opStack.append(token)
+			elif token['type'] == '(':
+				opStack.append(token)
+			elif token['type'] == ')':
+				while True:
+					if not opStack:
+						print 'expected more ('
+						return False
+					token = opStack.pop()
+					if token['type'] == '(':
+						break
+					evalStack.append(token)
 			else:
 				print 'Invalid syntax'
-		index += 1
-	return answer
+			parse_checkPrec(evalStack, opStack)
+	while opStack:
+		evalStack.append(opStack.pop())
+	# print evalStack
+	return evalStack
 
+def evaluate(evalStack):
+	evalStack.reverse()
+	tmpStack = []
+	while evalStack:
+		token = evalStack.pop()
+		if token['type'] == 'NUMBER':
+			tmpStack.append(token)
+		else:
+			if len(tmpStack) < 2:
+				print "Eval error (not enough operand)"
+				return False
+			elif token['type'] == 'OP' and token['op'] in evalTable:
+				tmpStack[-2]["val"] = evalTable[token['op']](
+						tmpStack[-2]["val"], tmpStack[-1]["val"])
+				tmpStack.pop()
+			else:
+				print 'Unknown operator', token['type']
+				return False
+		# print tmpStack
+	if(len(tmpStack) != 1):
+		print "Found extra operand"
+		return False
+	return tmpStack[0]["val"];
+
+def evalLine(line):
+	tokens = tokenize(line)
+	evalStack = parse(tokens)
+	if evalStack == False:
+		return False
+	return evaluate(evalStack)
 
 def test(line, expectedAnswer):
-	tokens = tokenize(line)
-	actualAnswer = evaluate(tokens)
+	actualAnswer = evalLine(line)
 	if abs(actualAnswer - expectedAnswer) < 1e-8:
 		print "PASS! (%s = %f)" % (line, expectedAnswer)
 	else:
@@ -71,6 +133,11 @@ def runTest():
 	print "==== Test started! ===="
 	test("1+2", 3)
 	test("1.0+2.1-3", 0.1)
+	test("1.0+2.1*3", 7.3)
+	test("3 + 2 * 6 / 3 * 5 - 2", 21)
+	test("(5 + 3) * 3 - 3 * (2 + 1)", 15)
+	test("13 % 6", 1)
+	test("2 ^ (1 + 3) * 3 - 5 * 2", 38)
 	print "==== Test finished! ====\n"
 
 runTest()
@@ -78,6 +145,6 @@ runTest()
 while True:
 	print '> ',
 	line = raw_input()
-	tokens = tokenize(line)
-	answer = evaluate(tokens)
-	print "answer = %f\n" % answer
+	answer = evalLine(line)
+	if(answer != False):
+		print "answer = %f\n" % answer
