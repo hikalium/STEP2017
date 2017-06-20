867f0babf9046fffcdac50a6320446deacc2c6b8
hikalium <hikalium@hikalium.com>
2017-06-20 23:55:16 +0900

    Update binrel.cpp

--- 04_graph/binrel.cpp
+++ 04_graph/binrel.cpp
@@ -4,55 +4,65 @@
 #include <sstream>
 #include <algorithm>
 
-const int num_of_pages = 1500000;  // >= 1483276
+const int max_pages = 1500000;  // >= 1483276
 
-int relCount[num_of_pages];
+int rels[64 * 1024 * 1024][2];	// [from, to]
 int num_of_rels = 0;
 
-int main(int argc, char *argv[])
+std::ifstream ifs_pages;
+std::string title[max_pages];
+int num_of_pages = 0;
+void scanPages()
+{
+	std::cout << "scanPages: begin" << std::endl;
+	ifs_pages.open("wikipedia_links/pages.txt");
+	while(!ifs_pages.eof()){
+		std::string line, token;
+		std::getline(ifs_pages, line);
+		std::istringstream stream(line);
+		std::string id;
+		getline(stream, id,'\t');
+		getline(stream, title[num_of_pages++],'\t');
+	}
+	std::cout << "scanPages: end" << std::endl;
+}
+
+void scanRels()
 {
 	std::ifstream ifs;
 	ifs.open("rel.bin");
 
+	std::cout << "scanRels: begin" << std::endl;
 	while(!ifs.eof()){
-		int fromID, toID;
-		ifs.read((char *)&fromID, sizeof(int));
-		ifs.read((char *)&toID, sizeof(int));
-		//std::cout << fromID << "," << toID << std::endl;
-		num_of_rels ++;
-		/*
-		if(fromID < num_of_pages) relCount[fromID]++;
-		if(lastFrom != fromID){
-			std::cout << lastFrom << "\t" << relCount[lastFrom] << std::endl;
-			lastFrom = fromID;
+		ifs.read((char *)&rels[num_of_rels][0], sizeof(int));
+		ifs.read((char *)&rels[num_of_rels][1], sizeof(int));
+		num_of_rels++;
+	}
+	std::cout << "scanRels: end" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	scanPages();
+	scanRels();
+	std::cout << "ready: rels = " << num_of_rels << std::endl;	
+	//
+	while(1){
+		int root_index;
+		std::cout << "Input root index: " << std::endl;
+		std::cin >> root_index;
+		if(root_index < 0 || num_of_pages <= root_index){
+			std::cout << "Out of bound." << std::endl;
+			break;
 		}
-		*/
-	}
-	std::cout << "ready: " << num_of_rels << std::endl;
-	/*
-	while(!ifs.eof()){
-		int fromID, toID;
-		ifs.read((char *)&fromID, sizeof(int));
-		ifs.read((char *)&toID, sizeof(int));
-		//std::cout << fromID << "," << toID << std::endl;
-		num_of_rels ++;
-		if(fromID < num_of_pages) relCount[fromID]++;
-		if(lastFrom != fromID){
-			std::cout << lastFrom << "\t" << relCount[lastFrom] << std::endl;
-			lastFrom = fromID;
+		std::cout << "root: " << root_index << ": " << title[root_index] << std::endl;
+		//
+		for(int i = 0; i < num_of_rels; i++){
+			if(rels[i][0] == root_index){
+				int to = rels[i][1];
+				std::cout << "\t" << to << "\t" << title[to] << std::endl;
+			}
 		}
-		
 	}
-	*/
-
-
-/*
-	int maxIndex = 0;
-	for(int i = 0; i <= num_of_pages; i++){
-		if(relCount[i] >= relCount[maxIndex]) maxIndex = i;
-	}
-
-	std::cout << "max: " << maxIndex << "\t" << relCount[maxIndex] << std::endl;
-*/
 	return 0;
 }
