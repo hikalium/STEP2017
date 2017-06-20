05e8332253cf397885fe4193ccdfc96296d5fb28
hikalium <hikalium@hikalium.com>
2017-06-20 23:08:39 +0900

    Fix

--- 04_graph/binrel.cpp
+++ 04_graph/binrel.cpp
@@ -7,19 +7,35 @@
 const int num_of_pages = 1500000;  // >= 1483276
 
 int relCount[num_of_pages];
+int num_of_rels = 0;
 
 int main(int argc, char *argv[])
 {
 	std::ifstream ifs;
 	ifs.open("rel.bin");
 
-	int lastFrom = 0;
 	while(!ifs.eof()){
 		int fromID, toID;
 		ifs.read((char *)&fromID, sizeof(int));
 		ifs.read((char *)&toID, sizeof(int));
 		//std::cout << fromID << "," << toID << std::endl;
-		
+		num_of_rels ++;
+		/*
+		if(fromID < num_of_pages) relCount[fromID]++;
+		if(lastFrom != fromID){
+			std::cout << lastFrom << "\t" << relCount[lastFrom] << std::endl;
+			lastFrom = fromID;
+		}
+		*/
+	}
+	std::cout << "ready: " << num_of_rels << std::endl;
+	/*
+	while(!ifs.eof()){
+		int fromID, toID;
+		ifs.read((char *)&fromID, sizeof(int));
+		ifs.read((char *)&toID, sizeof(int));
+		//std::cout << fromID << "," << toID << std::endl;
+		num_of_rels ++;
 		if(fromID < num_of_pages) relCount[fromID]++;
 		if(lastFrom != fromID){
 			std::cout << lastFrom << "\t" << relCount[lastFrom] << std::endl;
@@ -27,6 +43,9 @@ int main(int argc, char *argv[])
 		}
 		
 	}
+	*/
+
+
 /*
 	int maxIndex = 0;
 	for(int i = 0; i <= num_of_pages; i++){
